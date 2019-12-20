fc0b2e10b7b37c79165c5217e68de945f12d6826
alexzubc <alexzubc@gmail.com>
2019-12-20 12:46:03 +0700

    Initial commit

---
+++ src/academy/programming/Main.java
@@ -0,0 +1,20 @@
+package academy.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100d * 1.609344d);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+        // In the following that I will type below, write down what parts of the code
+        // are expressions.
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got high score");
+            score = 0;
+        }
+    }
+}
